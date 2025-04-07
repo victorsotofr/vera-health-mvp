b33233b0bf8eaa0c473879fb8929fdb567f18d0c
Victor Soto <sotovictor@outlook.fr>
2025-04-07 19:04:10 +0200

    Updates

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vera-health-mvp-soto.vercel.app/"],
+    allow_origins=["https://vera-health-mvp-soto.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
